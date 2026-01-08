4ea675293c2479651f1c6290bfe566f6852f0862
Yasser Boaventura <yasserboaventura78@gmail.com>
2026-01-08 11:28:20 +0000

    start

---
+++ src/test/java/com/ms/Emaill/EmaillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.Emaill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmaillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
